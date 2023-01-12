abeb37ca623ddff011e02a93de4268ba067f4a7b
blockodyssey-kjs <jskim@blockodyssey.io>
2023-01-12 14:34:15 +0900

    add: Header 검증 로직 구현

--- router/router.go
+++ router/router.go
@@ -2,6 +2,8 @@ package router
 
 import (
 	"lecture/go-wallet/controller"
+	"lecture/go-wallet/jwt"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,7 +12,7 @@ func GetRouter() *gin.Engine {
 	router := gin.Default()
 	router.GET("/health", controller.Health)
 	router.POST("/mnemonics", controller.NewMnemonicAndWallet)
-	router.POST("/signin/password", controller.SigninFromPassword)
+	router.POST("/signin/password", liteAuth(), controller.SigninFromPassword)
 	router.POST("/signin/mnemonic", controller.SigninFromMnemonic)
 	router.POST("/wallets", controller.NewWallet)
 	router.GET("/balances", controller.GetBalance)
@@ -19,4 +21,19 @@ func GetRouter() *gin.Engine {
 	router.GET("transactions", controller.GetTransactions)
 	router.GET("transactions/status", controller.GetTransactionStatus)
 	return router
+}
+
+func liteAuth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if c == nil {
+			c.Abort()
+			return
+		}
+		auth := c.GetHeader("Authorization")
+		splited := strings.Split(auth, "Bearer ")
+		mark := jwt.VerfyToken(splited[1])
+
+		c.AddParam("mark", mark)
+		c.Next()
+	}
 }
