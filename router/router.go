5100e22168d32b451a18848593deab00c73a8405
blockodyssey-kjs <jskim@blockodyssey.io>
2023-01-12 16:36:50 +0900

    add: CORS 처리

--- router/router.go
+++ router/router.go
@@ -9,26 +9,31 @@ import (
 )
 
 func GetRouter() *gin.Engine {
-	router := gin.Default()
+	r := gin.New()
+	r.Use(CORS())
+
 	// 헬스 체크
-	router.GET("/health", controller.Health)
+	r.GET("/health", controller.Health)
 	// 최초 니모닉 생성
-	router.POST("/mnemonics", controller.NewMnemonicAndWallet)
+	r.POST("/mnemonics", controller.NewMnemonicAndWallet)
 	// 패스워드 로그인
-	router.POST("/signin/password", liteAuth(), controller.SigninFromPassword)
+	r.POST("/signin/password", liteAuth(), controller.SigninFromPassword)
 	// 니모닉 로그인
-	router.POST("/signin/mnemonic", controller.SigninFromMnemonic)
+	r.POST("/signin/mnemonic", controller.SigninFromMnemonic)
 	// 지갑 추가
-	router.POST("/wallets", liteAuth(), controller.AddWallet)
+	r.POST("/wallets", liteAuth(), controller.AddWallet)
 	// 지갑 삭제
-	router.DELETE("/wallets", liteAuth(), controller.RemoveWallet)
+	r.DELETE("/wallets", liteAuth(), controller.RemoveWallet)
+	// 지갑 전체 조회
+	r.GET("/wallets", liteAuth(), controller.GetWallets)
+
 	// router.POST("/wallets", controller.NewWallet)
-	router.GET("/balances", controller.GetBalance)
-	router.POST("/wallets/valid", controller.CheckWalletValid)
-	router.POST("transfer/eth", controller.TransferETH)
-	router.GET("transactions", controller.GetTransactions)
-	router.GET("transactions/status", controller.GetTransactionStatus)
-	return router
+	// router.GET("/balances", controller.GetBalance)
+	// router.POST("/wallets/valid", controller.CheckWalletValid)
+	// router.POST("transfer/eth", controller.TransferETH)
+	// router.GET("transactions", controller.GetTransactions)
+	// router.GET("transactions/status", controller.GetTransactionStatus)
+	return r
 }
 
 func liteAuth() gin.HandlerFunc {
@@ -42,6 +47,20 @@ func liteAuth() gin.HandlerFunc {
 		mark := jwt.VerfyToken(splited[1])
 
 		c.AddParam("mark", mark)
+		c.Next()
+	}
+}
+
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, X-Forwarded-For, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
 		c.Next()
 	}
 }
